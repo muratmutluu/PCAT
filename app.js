7ad8bb963e6547b0347fefd11d07444cef646afc
Murat Mutlu <muratmutlu010@gmail.com>
2022-05-17 15:17:40 +0300

    updated

--- app.js
+++ app.js
@@ -1,17 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
-const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+// connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db');
 
 // Template Engine
 app.set('view engine', 'ejs');
 
+// Middlewares
 app.use(express.static('public'));
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Routes
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos,
+  });
+});
+
+app.get('/photos/:id', async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render('photo', {
+    photo,
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -20,6 +36,11 @@ app.get('/about', (req, res) => {
 
 app.get('/add', (req, res) => {
   res.render('add');
+});
+
+app.post('/photos', async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
